7cede3158b9ef54d3d3f7e7fd81d3417af42f6ab
iamkapil <kapilrhngdle2211@gmail.com>
2023-05-27 16:57:14 +0530

    signUp

--- Frontened/src/components/Signup.js
+++ Frontened/src/components/Signup.js
@@ -1,124 +1,128 @@
 //import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { toast } from "react-hot-toast";
 import { BiShow, BiHide } from "react-icons/bi";
-import {Link,  useHistory} from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 const Signup = () => {
   const navigate = useHistory();
-  
 
   const [data, setData] = useState({
-    firstName: "",
-    lastName: "",
+    firstname: "",
+    lastname: "",
     email: "",
     password: "",
     confirmPassword: "",
   });
 
-    const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-    const handleShowPassword = () => {
-      setShowPassword((preve) => !preve);
-    };
+  const handleShowPassword = () => {
+    setShowPassword((preve) => !preve);
+  };
 
-    const handleShowConfirmPassword = () => {
-      setShowConfirmPassword((preve) => !preve);
-    };
+  const handleShowConfirmPassword = () => {
+    setShowConfirmPassword((preve) => !preve);
+  };
 
-    const handleOnChange = (e) => {
-      const { name, value } = e.target;
-      setData((preve) => {
-        return {
-          ...preve,
-          [name]: value,
-        };
-      });
-    };
-  
-    console.log(process.env.REACT_APP_SERVER_DOMIN)
-    const handleSubmit= async(e)=>{
-      e.preventDefault();
-    const { firstName, email, password, confirmPassword } = data;
-    if (firstName && email && password && confirmPassword) {
-      if (password === confirmPassword) {
-    
-          const fetchData = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/signup`,{
-            method : "POST",
-            headers : {
-              "content-type" : "application/json"
-            },
-            body : JSON.stringify(data)
-          })
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
+    });
+  };
 
-          const dataRes = await fetchData.json()
-          console.log(dataRes)
+  console.log(process.env.REACT_APP_SERVER_DOMIN);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { firstname, lastname, email, password, confirmPassword } = data;
+    console.log(firstname,lastname,email, password,confirmPassword);
+    fetch("http://localhost:8080/signup",{
+      method:"POST",
+      crossDomain:true,
+      headers:{
+        "Content-type":"application/json",
+        Accept:"application/json",
+        "Access-Control-Allow-origin":"*",
+      },
+      body:JSON.stringify({
+        firstname,
+        lastname,
+        email,
+        password,
+        confirmPassword,
+      }),
+    })
 
-        // alert(dataRes.message);
-        // toast(dataRes.message)
-        // if(dataRes.alert){
-        //   navigate("/login");
-        // }
-       
-      } else {
-        alert("password and confirm password not equal");
-      }
-    } else {
-      alert("Please Enter required fields");
-    }
-    };
-
+    .then((res)=>res.json())
+    .then((data)=>{
+      console.log(data,"userRegister");
+    });
+  }
 
   return (
-      <div className=" d-flex  justify-content-center " style={{color : '#34495E ',  paddingTop:'3rem',}} >
-        <div style={{width:'25rem' ,paddingBottom:'5px', color:'#212F3D', backgroundColor:'#979A9A'}} >
-
+    <div
+      className=" d-flex  justify-content-center "
+      style={{ color: "#34495E ", paddingTop: "3rem" }}
+    >
+      <div
+        style={{
+          width: "25rem",
+          paddingBottom: "5px",
+          color: "#212F3D",
+          backgroundColor: "#979A9A",
+        }}
+      >
         <h2>SignUp</h2>
-          <form className="mb-3 mt-2 pl-2 pb-2" onSubmit={handleSubmit}>
-            <div>
-            <label htmlFor='firstname'>First name</label>
+        <form className="mb-3 mt-2 pl-2 pb-2">
+          <div>
+            <label htmlFor="firstname">First name</label>
             <input
-            type={"text"}
-            id="firstName"
-            name="firstName"
-            className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
-            value={data.firstName}
-            onChange={handleOnChange}
-            placeholder='Enter Your first name'
-          />
-            </div>
-            
-           <div>
-           <label htmlFor="lastName">Last Name</label>
-          <input
-            type={"text"}
-            id="lastName"
-            name="lastName"
-            className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
-            value={data.lastName}
-            onChange={handleOnChange}
-            placeholder='Enter Your last name'
-          />
-           </div>
+              type={"text"}
+              id="firstname"
+              name="firstname"
+              className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+              value={data.firstname}
+              onChange={handleOnChange}
+              placeholder="Enter Your first name"
+            />
+          </div>
 
-           <div>
-           <label htmlFor="email" className="form-label">Email</label>
-          <input
-            type={"email"}
-            id="email"
-            name="email"
-            className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
-            value={data.email}
-            onChange={handleOnChange}
-            placeholder='Enter Your Email'
-          />
-           </div>
-            
-           
-           <div >
-           <label htmlFor="password">Password</label>
+          <div>
+            <label htmlFor="lastname">Last Name</label>
+            <input
+              type={"text"}
+              id="lastname"
+              name="lastname"
+              className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+              value={data.lastname}
+              onChange={handleOnChange}
+              placeholder="Enter Your last name"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type={"email"}
+              id="email"
+              name="email"
+              className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+              value={data.email}
+              onChange={handleOnChange}
+              placeholder="Enter Your Email"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="password">Password</label>
             <input
               type={showPassword ? "text" : "password"}
               id="password"
@@ -126,7 +130,7 @@ const Signup = () => {
               className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
               value={data.password}
               onChange={handleOnChange}
-              placeholder='Enter Your password'
+              placeholder="Enter Your password"
             />
             <span
               className="flex text-xl cursor-pointer"
@@ -139,7 +143,7 @@ const Signup = () => {
 
          
           <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2  focus-within:outline focus-within:outline-blue-300">
-          <label htmlFor="confirmpassword">Confirm Password</label>
+            <label htmlFor="confirmpassword">Confirm Password</label>
             <input
               type={showConfirmPassword ? "text" : "password"}
               id="confirmpassword"
@@ -147,8 +151,7 @@ const Signup = () => {
               className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
               value={data.confirmPassword}
               onChange={handleOnChange}
-              placeholder='Confirm Your password'
-              
+              placeholder="Confirm Your password"
             />
             <span
               className="flex text-xl cursor-pointer"
@@ -158,18 +161,24 @@ const Signup = () => {
             </span>
           </div>
 
-          <button  type="button" className="btn btn-success" >submit</button>
-          </form>
-          <p className="text-left text-sm mt-2">
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={handleSubmit}
+          >
+            submit
+          </button>
+        </form>
+        <p className="text-left text-sm mt-2">
           Already have account ?{" "}
           <Link to={"/login"} className="text-red-500 underline">
             Login
           </Link>
         </p>
-        </div>
       </div>
+    </div>
   );
-}
+};
 
 export default Signup;
 
